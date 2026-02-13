3b135540c4eb851cd93399bcb6c0e6e89e55c35f
Michael Wood-Vasey <wmwv@pitt.edu>
2026-02-12 19:24:34 -0800

    Add nvcc tests.  Successfully fails for incompatible cuda+gcc.

--- tests/test_cuda.py
+++ tests/test_cuda.py
@@ -29,6 +29,8 @@ def test_raw_cuda_kernel():
 
     Using test from example at
     https://docs.cupy.dev/en/stable/user_guide/kernel.html
+
+    I think RawKernel uses nvrtc
     """
     import cupy as cp
 
@@ -51,3 +53,39 @@ void my_add(const float* x1, const float* x2, float* y) {
            [40., 42., 44., 46., 48.]], dtype=cp.float32)
 
     cp.testing.assert_allclose(y, expected, rtol=1e-7)
+
+
+def test_raw_cuda_kernel_nvcc():
+    """Can we use a raw kernel.  This is what SFFT uses.
+
+    Using test from example at
+    https://docs.cupy.dev/en/stable/user_guide/kernel.html
+
+    Here we use RawModule and specify the nvcc backend
+    """
+    import cupy as cp
+
+    code = r'''
+extern "C" __global__
+void my_add(const float* x1, const float* x2, float* y) {
+    int tid = blockDim.x * blockIdx.x + threadIdx.x;
+    y[tid] = x1[tid] + x2[tid];
+}
+'''
+
+    module = cp.RawModule(code=code, backend=u'nvcc', translate_cucomplex=False)
+    resamp_func = module.get_function('my_add')
+
+    x1 = cp.arange(25, dtype=cp.float32).reshape(5, 5)
+    x2 = cp.arange(25, dtype=cp.float32).reshape(5, 5)
+    y = cp.zeros((5, 5), dtype=cp.float32)
+    add_kernel((5,), (5,), (x1, x2, y))  # grid, block and arguments
+
+    expected = cp.array([[ 0.,  2.,  4.,  6.,  8.],
+           [10., 12., 14., 16., 18.],
+           [20., 22., 24., 26., 28.],
+           [30., 32., 34., 36., 38.],
+           [40., 42., 44., 46., 48.]], dtype=cp.float32)
+
+    cp.testing.assert_allclose(y, expected, rtol=1e-7)
+
