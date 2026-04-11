e680443bb8252333771b8f26735c27f6e84fde66
Michael Wood-Vasey <wmwv@pitt.edu>
2026-04-10 22:18:18 -0400

    ruff check tests

--- tests/test_cuda.py
+++ tests/test_cuda.py
@@ -7,7 +7,7 @@ is one of the tests.
 
 def test_import_cupy():
     """Can we import cupy."""
-    import cupy as cp
+    import cupy as cp  # noqa
     pass
 
 
@@ -21,7 +21,7 @@ def test_simple_cupy_array():
     import cupy as cp
 
     x = [100, 200]
-    y = cp.array(x)
+    _ = cp.array(x)
 
 
 def test_raw_cuda_kernel():
@@ -30,7 +30,7 @@ def test_raw_cuda_kernel():
     Using test from example at
     https://docs.cupy.dev/en/stable/user_guide/kernel.html
 
-    I think RawKernel uses nvrtc
+    RawKernel uses nvrtc
     """
     import cupy as cp
 
@@ -73,13 +73,13 @@ void my_add(const float* x1, const float* x2, float* y) {
 }
 '''
 
-    module = cp.RawModule(code=code, backend=u'nvcc', translate_cucomplex=False)
-    resamp_func = module.get_function('my_add')
+    module = cp.RawModule(code=code, backend="nvcc", translate_cucomplex=False)
+    resamp_func = module.get_function("my_add")
 
     x1 = cp.arange(25, dtype=cp.float32).reshape(5, 5)
     x2 = cp.arange(25, dtype=cp.float32).reshape(5, 5)
     y = cp.zeros((5, 5), dtype=cp.float32)
-    add_kernel((5,), (5,), (x1, x2, y))  # grid, block and arguments
+    resamp_func((5,), (5,), (x1, x2, y))  # grid, block and arguments
 
     expected = cp.array([[ 0.,  2.,  4.,  6.,  8.],
            [10., 12., 14., 16., 18.],
@@ -88,4 +88,3 @@ void my_add(const float* x1, const float* x2, float* y) {
            [40., 42., 44., 46., 48.]], dtype=cp.float32)
 
     cp.testing.assert_allclose(y, expected, rtol=1e-7)
-
