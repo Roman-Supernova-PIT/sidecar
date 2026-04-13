f7ffd7410fd75cec2ad98e85fa74d2c02d69fd9b
Michael Wood-Vasey <wmwv@pitt.edu>
2026-04-12 18:54:05 -0700

    Check returned shape in test_get_psf_kernel.

--- tests/test_get_psf_kernel.py
+++ tests/test_get_psf_kernel.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import numpy as np
 import pytest
 
 from snappl.image import FITSImageOnDisk, RomanDatamodelImage
@@ -17,4 +18,6 @@ testdata = [
 def test_get_psf_kernel(psf_type, psf_size):
     inpath = Path("/home/photometry_test_data/r9999901001001001001_0020_wfi02_f062_cal.asdf") 
     image = RomanDatamodelImage(inpath, no_base_path=True)
-    subtraction.get_psf_kernel(image, psf_type=psf_type, psf_size=psf_size)
+    stamp = subtraction.get_psf_kernel(image, psf_type=psf_type, psf_size=psf_size)
+    if psf_size is not None:
+        assert np.shape(stamp) == (psf_size, psf_size)
