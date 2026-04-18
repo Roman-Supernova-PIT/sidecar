15e106b42786e85f809456f7efca11e2e10c26f4
Michael Wood-Vasey <wmwv@pitt.edu>
2026-04-18 15:49:24 -0400

    Remove extra space at end of line.

--- tests/test_get_psf_kernel.py
+++ tests/test_get_psf_kernel.py
@@ -16,7 +16,7 @@ testdata = [
 
 @pytest.mark.parametrize("psf_type,psf_size", testdata)
 def test_get_psf_kernel(psf_type, psf_size):
-    inpath = Path("/home/photometry_test_data/r9999901001001001001_0020_wfi02_f062_cal.asdf") 
+    inpath = Path("/home/photometry_test_data/r9999901001001001001_0020_wfi02_f062_cal.asdf")
     image = RomanDatamodelImage(inpath, no_base_path=True)
     stamp = subtraction.get_psf_kernel(image, psf_type=psf_type, psf_size=psf_size)
     if psf_size is not None:
