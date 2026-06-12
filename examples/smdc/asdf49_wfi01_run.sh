59607d8bda92a84569e2d434d932f352f9bfc5ec
Michael Wood-Vasey <wmwv@pitt.edu>
2026-06-12 21:55:15 +0000

    Add ASDFThe49 script for SMDC.

---
+++ examples/smdc/asdf49_wfi01_run.sh
@@ -0,0 +1,26 @@
+cd /home/sidecar; pip install -e . --no-deps
+cd /home/snappl; pip install -e .
+cd /home/sfft; pip install -e . --no-deps
+cd /home
+
+# Can't reject known stars because we don't have a star catalog
+# This last combination of 99999010010010010010005 + F158 doesn't exist, so we only get 7 subtractions:
+for obsid in 99999010010010010010002 99999010010010010010003 99999010010010010010004 99999010010010010010005; do
+  for band in F106 F158; do
+    python \
+      /home/sidecar/sidecar/pipeline.py  \
+      --image-collection snpitdb \
+      --image-provenance-tag asdf_functional_test \
+      --image-process load_rdm_image \
+      --science-observation-id ${obsid} \
+      --science-band ${band} \
+      --science-sca 1 \
+      --template-observation-id 99999010010010010010001 \
+      --template-band ${band} \
+      --template-sca 1 \
+      --no-reject-known-stars \
+      --output-dir /sidecar_dia_out \
+      --temp-dir /dev/shm \
+      --backend4subtract numpy
+  done
+done
