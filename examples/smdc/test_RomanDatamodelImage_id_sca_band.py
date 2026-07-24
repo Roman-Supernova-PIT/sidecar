942cec57ca2d68d81e401d64e58c3558db9c19b2
Michael Wood-Vasey <wmwv@pitt.edu>
2026-07-24 18:58:33 +0000

    Improve test.

--- examples/smdc/test_RomanDatamodelImage_id_sca_band.py
+++ examples/smdc/test_RomanDatamodelImage_id_sca_band.py
@@ -1,3 +1,9 @@
+"""
+Test that we can read band, observation_id, sca from a particular RomandDatamodelImage
+
+You can check this by using less/more/cat/grep to just read the plain text of the ASDF header.
+"""
+
 from pathlib import Path
 
 from snappl.image import RomanDatamodelImage
@@ -7,5 +13,8 @@ science_image = Path("SNPIT_VISIT602000033_WFI01_F106_L2.asdf")
 
 science_image_path = base_path / science_image
 
-image = RomanDatamodelImage(full_filepath = science_image_path, no_base_path=True)
+science_image = RomanDatamodelImage(full_filepath = science_image_path, no_base_path=True)
 
+print(science_image.band)
+print(science_image.observation_id)
+print(science_image.sca)
