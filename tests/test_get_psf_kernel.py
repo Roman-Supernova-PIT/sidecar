3bd6b8b86c35eb214be20e118efaaa652fa6e628
Michael Wood-Vasey <wmwv@pitt.edu>
2026-07-29 23:24:23 +0000

    Make test_get_psf_kernel test depend on SNPIT_PHOTOMETRY_TEST_DATA_DIR being defined.

--- tests/test_get_psf_kernel.py
+++ tests/test_get_psf_kernel.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 import numpy as np
@@ -10,16 +11,25 @@ from sidecar import subtraction
 testdata = [
     ("gaussian", None),
     ("gaussian", 23),
-    ("STPSF", None,),
+    (
+        "STPSF",
+        None,
+    ),
     ("STPSF", 23),
 ]
 
-_rundir = Path(__file__).parent.resolve()
+SNPIT_PHOTOMETRY_TEST_DATA_DIR = os.getenv("SNPIT_PHOTOMETRY_TEST_DATA_DIR")
 
 
 @pytest.mark.parametrize("psf_type,psf_size", testdata)
+@pytest.mark.skipif(
+    SNPIT_PHOTOMETRY_TEST_DATA_DIR == None,
+    reason="Test relies on photometry_test_data",
+)
 def test_get_psf_kernel(psf_type, psf_size):
-    inpath = Path(_rundir) / Path("photometry_test_data/asdf_the_49/r9999901001001001001_0002_wfi01_f158_cal.asdf")
+    inpath = Path(SNPIT_PHOTOMETRY_TEST_DATA_DIR) / Path(
+        "asdf_the_49/r9999901001001001001_0002_wfi01_f158_cal.asdf"
+    )
     image = RomanDatamodelImage(inpath, no_base_path=True)
     stamp = subtraction.get_psf_kernel(image, psf_type=psf_type, psf_size=psf_size)
     if psf_size is not None:
